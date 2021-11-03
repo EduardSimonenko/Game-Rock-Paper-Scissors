d6f101c52cc13a9cc3af1896f8d924e22139e9f0
EduardSimonenko <90136070+EduardSimonenko@users.noreply.github.com>
2021-11-03 22:50:33 +0300

    game

--- src/Game.java
+++ src/Game.java
@@ -6,8 +6,9 @@ public class Game {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
 
+        getStrings(args);
         String key = GenKey.generator();
-        String[] str = getStrings();
+        String[] str = args;
         int computerMove = (int) (Math.random() * str.length);
 
         String hmac = GenKey.calculateHMAC(str[computerMove], key);
@@ -22,22 +23,16 @@ public class Game {
 
     }
 
-    static String[] getStrings() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the names of the moves  >= 3 ");
+    static void getStrings(String [] args) {
 
-        while (true) {
-            String str = in.nextLine();
-            String[] substr;
-            String delimeter = " ";
-            substr = str.split(delimeter);
-            if (substr.length >= 3 && substr.length % 2 != 0) {
-                return substr;
-            } else {
-                System.out.print("Enter correct values ");
-            }
+        if (args.length < 3 || args.length % 2 == 0) {
+            System.out.println("Error, count of elements must be >=3 and be odd number");
+            System.out.println("Example: rock paper scissors lizard Spock");
+
+            System.exit(0);
         }
     }
+
 
     static int getPlayerMove(String[] str) {
         Scanner scanner = new Scanner(System.in);
